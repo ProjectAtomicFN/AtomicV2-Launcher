ccff8ebbd2768ae09ab15ca970de3291c6f44f37
Vox <129524793+VoxyB89@users.noreply.github.com>
2024-08-31 22:39:03 +0300

    Create LauncherConfig.cs

---
+++ Services/Globals/LauncherConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atomic.Services.Globals
+{
+    public class LauncherConfig
+    {
+        public static string DiscordURl = "PUT YOUR DISCORD URI HERE"; // i'll change this in the far future
+
+        public static string LoginOauthApi = "PUT YOU IP OR DOMAN AND THEN : YOU PORT"; // Post and Get is different
+
+        public static string CurlDll = "AtomicCurl.dll"; // You will need to find a redirect as i won't provide
+    }
+}
